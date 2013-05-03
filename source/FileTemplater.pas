86f9b93ae987b7a9380a1deb9339c23d1c73b5c9
hinst <hinstance@yandex.ru>
2013-05-03 17:30:12 +0400

    .gitignore file added; fpc.cfg file added; FileTemplater: work in progress

--- source/FileTemplater.pas
+++ source/FileTemplater.pas
@@ -3,46 +3,100 @@ unit FileTemplater;
 interface
 
 uses
+  Types,
 	Classes,
 	
+	StringRoutines,
 	StreamReplacer;
 
 type
-
 	TTemplateReplacement = class(TComponent)
+	public
 		What: String;
-		function Write(const aOutput: TStream): boolean; virtual;
+		function Write(const aOutput: TStream): Boolean; virtual;
 	end;
 	
+type
 	TTemplateStringReplacement = class(TTemplateReplacement)
 	public
 		Text: String;
-		function Write(const aOutput: TStream): boolean; override;
+		function Write(const aOutput: TStream): Boolean; override;
 	end;
 	
+type
 	TTemplateFileReplacement = class(TTemplateReplacement)
 	public
 		FileName: String;
+		function Write(const aOutput: TStream): Boolean; override;
 	end;
 
-	TFileTemplater = class(TComponent)
+type
+
+{ TFileTemplater }
+
+ TFileTemplater = class(TComponent)
 	protected
-		FReplacements: TList;
+		FReplacers: TList;
+    FReplacer: TStreamReplacer;
+    FFileName: TStringDynArray; // 0 is input, 1 is output
 	public
-		constructor Create(const aOwner: TComponent);
-		procedure AddReplacement(const aReplacement: TTemplateReplacement);
+		constructor Create(aOwner: TComponent); override;
+		procedure AddReplacer(const aReplacer: TTemplateReplacement);
+    destructor Destroy; override;
 	end;
 
 implementation
 
-constructor TFileTemplater.Create(const aOwner: TComponent);
+function TTemplateReplacement.Write(const aOutput: TStream): Boolean;
 begin
-	inherited Create(aOwner);
+  aOutput.Write(PChar(What)^, GetByteLength(What));
 end;
 
-procedure TFileTemplater.AddReplacement(const aReplacement: TTemplateReplacement);
+function TTemplateStringReplacement.Write(const aOutput: TStream): Boolean;
 begin
-	
+	aOutput.Write(PChar(Text)^, GetByteLength(Text));
+end;
+
+function TTemplateFileReplacement.Write(const aOutput: TStream): Boolean;
+var
+  stream: TFileStream;
+begin
+  result := FileName <> '';
+  if
+    result
+  then
+  begin
+    stream := TFileStream.Create(FileName, fmOpenRead);
+    aOutput.CopyFrom(stream, stream.Size);
+    stream.Free;
+  end;
+end;
+
+{ TFileTemplater }
+
+constructor TFileTemplater.Create(aOwner: TComponent);
+begin
+	inherited Create(aOwner);
+	FReplacers := TList.Create;
+  SetLength(FFileName, 2);
+end;
+
+procedure TFileTemplater.AddReplacer(const aReplacer: TTemplateReplacement);
+begin
+	FReplacers.Add(aReplacer);
+end;
+
+destructor TFileTemplater.Destroy;
+begin
+  SetLength(FFileName, 0);
+  FReplacers.Free;
+  inherited Destroy;
 end;
 
 end.
+
+
+
+
+
+
